e642da27ed05a94bbaa3599b1b02c33bbd831793
edithopara <edithopara1@gmail.com>
2023-02-16 00:23:07 +0100

    javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+function multiply (value1, value2)
+ {return value1 * value2}
+ console.log(multiply(10, 5))
+
+
+ let firstName = 'Edith';
+ let lastName = 'Opara';
+ let track = 'Frontend Engineer';
+ let skill = 'Cooking';
+ let color = 'pink';
+ let independence = '1960';
+
+//concatenation
+console.log("My name is " + firstName + lastName + ". I am a " + track + ". I love " + skill + ". My favorite color is "
+ + color + ". My country gained independence in " + independence)
+
